889f4c0a4e5449e7afc48ec2638883ce16fae0b2
Gilberto Hernandez <20201171@uthh.edu.mx>
2025-12-15 16:11:28 -0600

    commit inicial

---
+++ app.ts
@@ -0,0 +1,28 @@
+console.log("Hola Mundo");
+
+type User = {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+};
+const user: User = {
+    id: 1,
+    name: "cesar",
+    email: "cesar@gmail.com",
+    password: "123456",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+};
+
+function getUser(id: number): User | undefined {
+    if (id === user.id) {
+        return user;
+    } else {
+        return undefined;
+    }
+}
+
+//console.log(getUser(user.id));
